326b575fabd97ca8e959c943d8554f7f1ab0b847
Carlos Alarcon <jchuerva@github.com>
2021-05-31 17:11:57 +0200

    use ruby script

---
+++ .github/workflows/main.rb
@@ -0,0 +1,45 @@
+require "octokit"
+require "json"
+
+REPO = "jchuerva/test_app_1'"
+
+FAILED_MESSAGE = <<HEREDOC
+  This file currently does not belong to a service. To fix this, please do one of the following:
+    * Find a service that makes sense for this file and update SERVICEOWNERS accordingly
+    * Create a new service and assign this file to it
+
+  Learn more about service maintainership here:
+  https://thehub.github.com/engineering/development-and-ops/dotcom/serviceowners
+HEREDOC
+
+def print_message_in_files(files)
+  files.each do |file|
+    print "::warning file=#{file}::#{FAILED_MESSAGE}"
+  end
+end
+
+def get_pr_files
+  client = Octokit::Client.new(access_token: TOKEN)
+  response = client.pull_request_files(REPO, PR_NUMBER)
+  response.map { |file| file["filename"] }
+end
+
+files = get_pr_files
+
+return unless files
+binding.pry
+
+serviceownes_no_match = File.read("serviceowners_no_matches.txt")
+unowned_files = []
+
+files.each do |file|
+  if serviceownes_no_match.include?(file)
+    unowned_files << file
+  end
+
+  if unowned_files
+    print_message_in_files(unowned_files)
+  else
+    print "Looks good! All files modified have an owner!"
+  end
+end
