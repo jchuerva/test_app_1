15d89dc881efc901ff591cfd6b8e543ab3f2298b
Carlos Alarcon <jchuerva@github.com>
2021-06-02 12:10:25 +0200

    Update main.rb

--- .github/workflows/main.rb
+++ .github/workflows/main.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
-require "octokit"
-require "json"
-require "erb"
 
-github_token = ENV["GITHUB_TOKEN"]
-repo = ENV["REPO"]
-pr_number = ENV["PR_NUMBER"]
+require 'octokit'
+require 'json'
+require 'erb'
 
+github_token = ENV['GITHUB_TOKEN']
+repo = ENV['REPO']
+pr_number = ENV['PR_NUMBER']
 
 class UnownedFileParser
   def initialize(repo, pr_number, github_token)
@@ -16,7 +16,7 @@ class UnownedFileParser
   end
 
   def self.build_fail_message
-    message = <<-HEREDOC
+    message = <<~HEREDOC
       This file currently does not belong to a service. To fix this, please do one of the following:
 
         * Find a service that makes sense for this file and update SERVICEOWNERS accordingly
@@ -41,20 +41,19 @@ class UnownedFileParser
        <https://thehub.github.com/engineering/development-and-ops/dotcom/serviceowners/service-oriented-maintainership/>
     HEREDOC
 
-    message.gsub("\n", "%0A").freeze
+    message.gsub("\n", '%0A').freeze
   end
 
   def self.build_fail_message_alt
-    "
-      Alt \n\
-      This file currently does not belong to a service. To fix this, please do one of the following: \n\
-      \n
-      \t* Find a service that makes sense for this file and update SERVICEOWNERS accordingly \n\
-      \t* Create a new service and assign this file to it \n\
-      \n
-      \nLearn more about service maintainership here: \
-      \n<https://thehub.github.com/engineering/development-and-ops/dotcom/serviceowners/service-oriented-maintainership/> \
-    "
+    %(Alt
+      This file currently does not belong to a service. To fix this, please do one of the following:
+
+      * Find a service that makes sense for this file and update SERVICEOWNERS accordingly
+      * Create a new service and assign this file to it
+
+      Learn more about service maintainership here:
+      <https://thehub.github.com/engineering/development-and-ops/dotcom/serviceowners/service-oriented-maintainership/>
+      )
   end
 
   FAIL_MESSAGE = build_fail_message
@@ -62,7 +61,7 @@ class UnownedFileParser
   FAIL_MESSAGE_ALTERNATIVE = build_fail_message_alt.freeze
 
   def puts_message_in_files(files)
-    puts "This PR touches some unowned files"
+    puts 'This PR touches some unowned files'
     files.each do |file|
       puts "::warning file=#{file}::#{FAIL_MESSAGE}"
       puts "::warning file=#{file}::#{FAIL_MESSAGE_ORI}"
@@ -76,12 +75,12 @@ class UnownedFileParser
 
   def get_pr_files
     response = octokit_client.pull_request_files(@repo, @pr_number)
-    response.map { |file| file["filename"] }
+    response.map { |file| file['filename'] }
   end
 
   def get_unowned_files(files)
     unowned_files = []
-    serviceownes_no_match = File.read("docs/serviceowners_no_matches.txt").lines.map(&:chomp)
+    serviceownes_no_match = File.read('docs/serviceowners_no_matches.txt').lines.map(&:chomp)
 
     files.each do |file|
       unowned_files << file if serviceownes_no_match.include?(file)
@@ -93,13 +92,14 @@ class UnownedFileParser
   def run
     files = get_pr_files
     return unless files
+
     unowned_files = get_unowned_files(files)
 
     if unowned_files.any?
       puts_message_in_files(unowned_files)
-      raise "Unowned files found"
+      raise 'Unowned files found'
     else
-      puts "Looks good! All files modified have an owner!"
+      puts 'Looks good! All files modified have an owner!'
     end
   end
 end
